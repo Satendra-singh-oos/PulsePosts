cc469b2388b8002cc40f772ec0691514ebb4641e
Satendra Singh <satendrasingh1325@gmail.com>
2024-02-28 16:43:40 +0530

    added Rate limiter

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { rateLimit } from "express-rate-limit";
+import requestIp from "request-ip";
 
 const app = express();
 
@@ -10,6 +12,29 @@ app.use(
     credentials: true,
   })
 );
+
+app.use(requestIp.mw());
+
+// Rate limiter to avoid misuse of the service and avoid cost spikes
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5000, // Limit each IP to 500 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  keyGenerator: (req, res) => {
+    return req.clientIp; // IP address from requestIp.mw(), as opposed to req.ip
+  },
+  handler: (_, __, ___, options) => {
+    throw new ApiError(
+      options.statusCode || 500,
+      `There are too many requests. You are only allowed ${
+        options.max
+      } requests per ${options.windowMs / 60000} minutes`
+    );
+  },
+});
+// Apply the rate limiting middleware to all requests
+app.use(limiter);
 
 app.use(express.json({ limit: "20kb" }));
 app.use(express.urlencoded({ extended: true, limit: "20kb" }));
@@ -22,6 +47,7 @@ import healthcheckRouter from "./routes/healthcheck.routes.js";
 import userRouter from "./routes/users.routes.js";
 import blogRouter from "./routes/blogs.routes.js";
 import followRouter from "./routes/follows.routes.js";
+import { ApiError } from "./utils/ApiError.js";
 
 app.use("/api/v1/healthcheck", healthcheckRouter);
 
